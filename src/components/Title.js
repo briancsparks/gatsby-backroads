c60bc67228608a611648aa2002512a52cd1a4e89
Brian C Sparks <brian.c.sparks@hp.com>
2020-03-19 01:14:15 -0700

    alternate syntax

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,7 +1,7 @@
 import React        from 'react'
 import styled       from 'styled-components'
 
-const Title = ({title,subtitle}) => {
+const Title = ({title, subtitle}) => {
   return (
     <TitleWrapper>
       <h4>
@@ -36,4 +36,56 @@ const TitleWrapper = styled.div`
   }
 `
 
-export default Title
+export default Title;
+
+
+
+
+
+
+
+
+
+
+// Could not get this to work (from 106. Alternate Syntax):
+
+// const Title = ({ title, subtitle, className }) => {
+//   return (
+//     <div className={className}>
+//       <h4>
+//         <span className="title">{title}</span>
+//         <span>{subtitle}</span>
+//       </h4>
+//     </div>
+//   )
+// }
+
+// export default styled(Title)`
+//   text-transform:   uppercase;
+//   font-size:        2.3rem;
+//   margin-bottom:    2rem;
+
+//   h4{
+//     text-align:     center;
+//     letter-spacing: 7px;
+//     color:          var(--primaryColor);
+//   }
+//   .title {
+//     color:          var(--mainBlack);
+//   }
+//   span {
+//     display:        block;
+//   }
+//   @media (min-width:576px) {
+//     span {
+//       display:      inline;
+//       margin:       0 0.35rem;
+//     }
+//   }
+// `
+
+// // export default Title;
+
+
+
+
