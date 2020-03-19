9ac75dde8daed8da654d53a45d53cfeba590180c
Brian C Sparks <brian.c.sparks@hp.com>
2020-03-19 00:19:41 -0700

    styled-component Button example

--- src/pages/tours.js
+++ src/pages/tours.js
@@ -1,11 +1,17 @@
 import React, { Component }   from 'react'
 import Layout                 from '../components/Layout'
+import Button                 from '../examples/Button'
 
 export default class tours extends Component {
   render() {
     return (
       <Layout>
         Hello from tours page!
+        <div>
+          <Button>
+            hello from styled
+          </Button>
+        </div>
       </Layout>
     )
   }
