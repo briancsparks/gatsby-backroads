2d029f292ff1bce6aa9bbbbf7cad8bff00b1050b
Brian C Sparks <brian.c.sparks@hp.com>
2020-03-19 04:23:14 -0700

    page query for blog

--- src/pages/tours.js
+++ src/pages/tours.js
@@ -1,15 +1,10 @@
 import React, { Component }   from 'react'
 import Layout                 from '../components/Layout'
-// import Header                 from '../examples/Header'
-import Header                 from '../examples/RegularHeader'
 
 export default class tours extends Component {
   render() {
     return (
-      <Layout>
-        Hello from tours page!
-        <Header />
-      </Layout>
+      <Layout> Hello from tours page! </Layout>
     )
   }
 }
